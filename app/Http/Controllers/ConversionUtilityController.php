e9981e1be11d3bf43dbe2fea47d6eb8ef3b86e10
ArrDulce <22150092@aguascalientes.tecnm.mx>
2026-05-23 13:53:13 -0600

    dulce - arch

---
+++ app/Http/Controllers/ConversionUtilityController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ConversionUtilityController extends Controller
+{
+    public function cToF(float $c): float
+    {
+        return ($c * 9 / 5) + 32;
+    }
+}
