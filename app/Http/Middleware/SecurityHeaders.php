48206a8df0f6d105fd6c70d2188be6de0cdd811d
itzelvalrey28 <itzelvalrey28@gmail.com>
2026-05-23 20:07:18 -0600

    modificacion en estilo

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -13,7 +13,7 @@ class SecurityHeaders
         $response = $next($request);
 
         // Bloqueo de inyecciones externas y protección de sniffing
-        $response->headers->set('Content-Security-Policy', "default-src 'self'; script-src 'self'; object-src 'none';");
+        $response->headers->set('Content-Security-Policy', "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; object-src 'none';");
         $response->headers->set('X-Frame-Options', 'DENY');
         $response->headers->set('X-Content-Type-Options', 'nosniff');
 
