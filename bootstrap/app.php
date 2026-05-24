8b87c2ce9949b2fcbab76b782f41ee3a7b5e4c1d
itzelvalrey28 <itzelvalrey28@gmail.com>
2026-05-23 20:00:51 -0600

    modificacion en seguridad

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,14 +1,9 @@
  <?php
 
-
-
+use App\Http\Middleware\SecurityHeaders;
 use Illuminate\Foundation\Application;
-
 use Illuminate\Foundation\Configuration\Exceptions;
-
 use Illuminate\Foundation\Configuration\Middleware;
-
-
 
 return Application::configure(basePath: dirname(__DIR__))
 
@@ -36,6 +31,6 @@ return Application::configure(basePath: dirname(__DIR__))
 
     ->withMiddleware(function (Middleware $middleware) {
 
-    $middleware->append(\App\Http\Middleware\SecurityHeaders::class);
+        $middleware->append(SecurityHeaders::class);
 
-})->create();
+    })->create();
